1275162684ab602044250c7333ddaf7ca00113d2
jharnakumari <jharna.maheshwari496@gmail.com>
2020-06-14 17:06:01 +0500

    chapter 1

---
+++ chapter 1/app.js
@@ -0,0 +1,276 @@
+// -------------Chapter # 3---------------
+
+// ---------------Q1-------------
+
+// var age = "I am 21 years old";
+// alert(age)
+
+// ----------Q3-----------
+// var N = prompt("Enter your name");
+// alert("you have visited this site multiple times")
+
+// -----------Q4-------------
+// var bdayyear = "1997"
+// document.write("my birthday is" + bdayyear + "<br>" + "data type of my declared variable is number")
+
+// -----------------Q4------------------
+
+// var name = prompt("Enter your name")
+
+// var quantity = prompt("Enter Quantity")
+
+// var product = prompt("Enter product title ")
+
+// var store = prompt("Enter store name")
+
+// var message = name.bold() + " " + "ordered" + " " + quantity.bold() + " " + product.bold() + " " + "on" + " " + store;
+
+// document.write(message);
+
+
+
+// --------------------Chapter#4-------------------
+
+// ------------------Q1-------------------
+
+// var x, y, z;
+
+
+// -----------Q2-------------
+
+//---------illegal----------
+// var 76names
+// var @%surname
+// var name&Code
+// var 12city
+// var +89lastname
+
+// ---------legal---------
+// var name_99
+// var surname_code
+// var a
+// var variable_name
+// var your_name
+
+// ----------Q3-------------
+// var head = "Rules for naming JS variables"
+// document.write(head.bold())
+
+// var message = "<br>" + "<br>" + "<br>" + "Variable names can only contains" + " " + "Numbers," + " " + "Alpha_numeric," + " " + "and" + " " + "_." + " " + "For example $my_1stVariable" + 
+// "<br>" + "Variables must begin with a" + " " + "letters," + "$" + "and" + "_." + "For example $name, _name or name" + 
+// "<br>" + "Variable names are case" + " " + "sensitive." + "<br>" + "Variable names should not be JS" + " " + "KeyWord.";
+
+// document.write(message)
+
+
+
+// ---------------Chapter#5-------------
+
+// ----------Q1----------
+
+// var a = 387;
+// var b = 254;
+// var result = a + b;
+// document.write("Sum of 387 and 254 is" + " " + result)
+
+// -----------Q2--------------
+
+// var a = 387;
+// var b = 254;
+// var result1 = a + b;
+// var result2 = a - b;
+// var result3 = a * b;
+// var result4 = a / b;
+// var result5 = a % b;
+// document.write("Sum of 387 and 254 is" + " " + result1)
+// document.write("<br>" + "Subtract of 387 and 254 is" + " " + result2)
+// document.write("<br>" + "Multiply of 387 and 254 is" + " " + result3)
+// document.write("<br>" + "Division of 387 and 254 is" + " " + result4)
+// document.write("<br>" + "Modules of 387 and 254 is" + " " + result5)
+
+// ---------Q3-----------
+
+// var x;
+// x = "Value after variable declaration is";
+// document.write(x + " " + "undefined")
+
+// var x = 8;
+// document.write("<br>" + "Initial value is:" + " " + x)
+
+// var x = 5;
+// document.write("<br>" + "Value after increment is:" + " " +(++x))
+
+// var x = 7;
+// var y = 6;
+// var z = x + y;
+// document.write("<br>" + "Value after addition is:" + " " + z )
+
+// var x = 13;
+// document.write("<br>" +  "Value after decrement is:" + " " +(--x))
+
+// var x = 0;
+// var y = 3;
+// var z = x / y;
+// document.write("<br>" + "The remainder is:" + " " + z )
+
+// -----------Q4----------------
+
+// var ticket_prize = 1000;
+// x = ticket_prize * 5;
+// document.write("Total cost to buy 5 tickets to a movie is" + " "+ x + "PKR")
+
+// ----------Q5--------------
+// for(var i = 1; i <= 10; i++){
+//     document.write("8" + "x" + i + "=" + 8*i + "<br />")
+// }
+
+// ------------Q6----------
+// var C = 25;
+// var F = 70;
+// cal = (F - 32) * 5 / 9;
+// far = (C * 9 / 5) + 32;
+// document.write(C + "&#8451; is" + " " + far + "&#8457;" + "<br>")
+// document.write(F + "&#8451; is" + " " + cal + "&#8457;")
+
+// -------------Q7------------
+
+// var head = "Shopping Cart";
+// var prize1 = 1000;
+// var prize2 = 600;
+// var quantity1 = 4;
+// var quantity2 = 8;
+// var charges = 200;
+// var total = prize1*quantity1 + prize2*quantity2 + charges;
+
+// document.write(head.bold())
+// document.write("<br>" + "<br>" + "<br>" + "Prize of item 1 is" + " " + prize1)
+// document.write("<br>" + "Quantity of item 1 is" + " " + quantity1)
+// document.write("<br>" + "Prize of item 2 is" + " " + prize2)
+// document.write("<br>" + "Quantity of item 2 is" + " " + quantity2)
+// document.write("<br>" + "Shipping Charges is" + " " + charges)
+// document.write("<br>" + "<br>" + "Total cost of your order is" + " " + total)
+
+// -------Q8------------
+
+// var marks = 100;
+// var obtained = 85;
+// var result = (obtained/marks)*100;
+
+// document.write("Total marks:" + " " + marks)
+// document.write("<br>" + "Marks Obtained:" + " " + obtained)
+// document.write("<br>" + "Percentage:" + " " + result + " " + "%")
+
+// -----------Q9-------------
+
+// var head = "Currency in PKR";
+// var dollar = 10;
+// var riyal = 25;
+// var dollar_pak = 10*104.8;
+// var riyal_pak = 25*28;
+// var total = dollar_pak + riyal_pak;
+
+// document.write(head.bold())
+// document.write("<br>" + "<br>" + "<br>" + "Total currency in PKR:" + " " + total)
+
+// ---------Q10-----------
+
+// var x = 6;
+// var y = 5;
+// var z = x + y;
+// var a = z*10;
+// var b = a/2;
+// document.write(z + "<br>"  + " " + a + "<br>" + " " + b)
+
+// ----------Q11-------------
+
+// var head = "Age Calculator";
+// var current_year = 2020;
+// var Birth_year = 1999;
+// var age = current_year - Birth_year;
+
+// document.write(head.bold())
+// document.write("<br>" + "<br>" + "current year:" + " " + current_year + "<br>" + "Birth year:" + " " + Birth_year + "<br>" + 
+// "Your age is:" + " " + age)
+
+// ---------------Q12-------------
+
+// var head = " The Geometrizer";
+// var r = 20;
+// var pi = 3.42;
+// var Circumference_of_circle = 2 * pi * r;
+// var area_of_circle = pi* (r^2);
+
+// document.write(head.bold())
+// document.write("<br>" + "<br>" + "Radius of the circle:" + " " + r + "<br>" + "Circumference of a circle:" + " " + Circumference_of_circle +
+// "<br>" + "area of the circle:" + " " + area_of_circle)
+
+// ---------Q13-----------
+
+// var head = "The life time supply calculator";
+// var snack = "Kitkat chocolate";
+// var age = 21;
+// var max_age = 80;
+// var est_amount = 2;
+// var total = (max_age - age) * 2;
+
+// document.write(head.bold())
+// document.write("<br>" + "<br>" + "Favourite Snack:" + " " + snack + "<br>" + "Current Age:" + " " + age + "<br>" +
+// "Estimated Maximum Age:" + " " + max_age + "<br>" + "Amount of snack per day:" + " " + est_amount + 
+// "<br>" + "You will need" + " " + total + " " + "Kitkat Chocolate to last you until the rip old age of" + " " +
+// max_age)
+
+
+// ----------Chapter#6-9----------
+
+// -----------Q1-------------
+
+// var a = 10;
+// document.write("Result:" + "<br>" + "The value of a is:" + " " + a)
+
+// document.write("<br>" + "<br>" + ".....................................")
+
+// var a=10;
+// document.write("<br>" + "<br>" + "The value of ++a is:" + " ", ++a)
+// document.write("<br>" + "Now the value of a is:" + " ", a++)
+
+// var a = 10;
+// document.write("<br>" + "<br>" + "The value of a++ is:" + " ", ++a)
+// document.write("<br>" + "Now the value of a is:" + " ", ++a)
+
+// document.write("<br>" + "<br>" + "The value of --a is:" + " ", --a)
+// document.write("<br>" + "Now the value of a is:" + " ", a++)
+
+// document.write("<br>" + "<br>" + "The value of a-- is:" + " ", --a)
+// document.write("<br>" + "Now the value of a is:" + " ", --a)
+
+// -------------Q2-------------
+
+// var a = 5;
+// var b = 4;
+// var result = --a - --b + ++b + b--;
+
+// document.write("a is:" + " " + a + "<br>" + "b is:" + " " + b + "<br>" + " " + "Result:" + " " + result)
+
+// document.write("<br>" + "<br>", --a)
+// document.write("<br>", --a - --b)
+// document.write("<br>", --a - --b + ++b)
+// document.write("<br>", --a - --b + ++b + b--)
+
+// ------------Q3----------
+
+// var name = prompt("Entr your name");
+
+// alert("Welcome into our Website" + " " + name)
+
+// ------------Q4--------------
+
+// var num = prompt("Enter number");
+
+// for(let i = 1; i <= 10; i++){
+// document.write(num + "x" + i + "=" + num*i + "<br />")
+// }
+// for(let j = 1; j <= 10; j++){
+//     document.write("5" + "x" + j + "=" + 5*j + "<br>")
+//  }
+
+// ---------------Q4----------
